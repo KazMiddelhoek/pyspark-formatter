a4dab2389b711efde08625f3f5aa46517b9707ad
Kaz <kaz.middelhoek@ns.nl>
2024-11-15 16:51:22 +0100

    [skip ci]

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="pyspark_formatter",
     packages=find_packages(),
+    install_required=open("requirements.txt").read().strip().split("\n"),
     entry_points={"console_scripts": ["pyspark-formatter=pyspark_formatter.main:run"]},
 )
