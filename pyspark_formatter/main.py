b6c789838a95e4b06d9b3d5971099041f2166d73
Kaz <kaz.middelhoek@ns.nl>
2024-11-15 15:49:12 +0100

    [skip ci] add integration test

--- pyspark_formatter/main.py
+++ pyspark_formatter/main.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import libcst as cst
 from collections.abc import Sequence
 from argparse import ArgumentParser
---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+from pyspark_formatter import main
+
+def test_main(tmp_path) -> None:
+    tmp_file = tmp_path / "tmp.py"
+    tmp_file.write_text("""w = Window().orderBy(["a"]).partitionBy(["b"])
+df1.join(df2, ["c"]).select(f.col("c"))""")
+
+    main.run([str(tmp_file)])
+
+    with open(tmp_file) as f:
+        result = f.read()
+    assert result == """w = Window().orderBy("a").partitionBy("b")
+df1.join(df2, "c").select("c")"""
