32306729164c453c71747220c83eb1ad058fc4c8
Kaz <kaz.middelhoek@ns.nl>
2024-11-15 13:17:18 +0100

    [skip ci]

--- tests/test_window_formatter.py
+++ tests/test_window_formatter.py
@@ -1,5 +1,14 @@
 from pyspark_formatter.window_formatter import PysparkWindowTransformer
 import libcst as cst
+from libcst.metadata import MetadataWrapper
+
+
 def test_window_formatter():
-    module =cst.parse_module('Window.partitionBy(["a", "b"]).orderBy(["c"], "d")')
-    assert module.visit(PysparkWindowTransformer()).code == 'Window.partitionBy("a", "b").orderBy("c", "d")'
+    module = cst.parse_module("""Window.partitionBy(["a", "b"]).orderBy(["c"], "d")                    
+                              """)
+    wrapper = MetadataWrapper(module)
+    print(wrapper)
+    assert (
+        wrapper.visit(PysparkWindowTransformer()).code
+        == 'Window.partitionBy("a", "b").orderBy("c", "d")'
+    )
