838218c0d29503c4357142d1ee83c7ca3c9ffded
Kaz <kaz.middelhoek@ns.nl>
2024-11-15 15:36:17 +0100

    [skip ci]

--- pyspark_formatter/window_formatter.py
+++ pyspark_formatter/window_formatter.py
@@ -1,25 +1,23 @@
-# import libcst as cst
-# from libcst.metadata import ParentNodeProvider
+import libcst as cst
+import libcst.helpers
+import libcst.matchers as m
 
 
-# class PysparkWindowTransformer(cst.CSTTransformer):
-#     METADATA_DEPENDENCIES = (ParentNodeProvider,)
-
-#     # def leave_Call(self, original_node: cst.Call, updated_node: cst.Call) -> cst.Call:
-#     #     print(dump(updated_node))
-#     #     if (m.matches(updated_node.func.value, m.Name(value="Window"))
-#     #        # m.matches(updated_node.func.value.func.value, m.Name(value="Window"))
-#     #     )  and m.matches(updated_node.func.attr, m.Name("partitionBy") | m.Name("orderBy")):
-#     #         for i, arg in enumerate(original_node.args):
-#     #             if isinstance(arg.value, cst.List):
-#     #                 #args_with_unpacked_list = updated_node.args[:i]+tuple(cst.Arg(value=elem.value) for elem in arg.value.elements)+ updated_node.args[i+1:]
-#     #                 updated_node = updated_node.with_deep_changes(updated_node.args[i], value=cst.FlattenSentinel(tuple(cst.Arg(value=elem.value) for elem in arg.value.elements)))
-#     #     return updated_node
-
-#     def leave_Arg(self, original_node: cst.List, updated_node: cst.List) -> cst.List:
-#         if isinstance(updated_node.value, cst.List):
-#             # test =self.get_metadata(ParentNodeProvider, original_node)
-#             return cst.FlattenSentinel(
-#                 tuple(cst.Arg(value=elem.value) for elem in updated_node.value.elements)
-#             )
-#         return updated_node#
+class PysparkWindowTransformer(cst.CSTTransformer):
+    def leave_Call(self, original_node: cst.Call, updated_node: cst.Call) -> cst.Call:
+        if m.matches(
+            updated_node,
+            m.Call(
+                func=m.Attribute(
+                    attr=m.Name(value="partitionBy") | m.Name(value="orderBy")
+                ),
+                args=(m.Arg(value=m.List()),),
+            ),
+        ) and "Window." in libcst.helpers.get_full_name_for_node(updated_node):
+            updated_node = updated_node.with_changes(
+                args=tuple(
+                    cst.Arg(value=elem.value)
+                    for elem in updated_node.args[0].value.elements
+                )
+            )
+        return updated_node
--- tests/test_window_formatter.py
+++ tests/test_window_formatter.py
@@ -1,14 +1,13 @@
 from pyspark_formatter.window_formatter import PysparkWindowTransformer
 import libcst as cst
-from libcst.metadata import MetadataWrapper
 
 
 def test_window_formatter():
-    module = cst.parse_module("""Window.partitionBy(["a", "b"]).orderBy(["c"], "d")                    
-                              """)
-    wrapper = MetadataWrapper(module)
-    print(wrapper)
+    module = cst.parse_module("""Window.partitionBy(["a", "b"]).orderBy("c")
+pyspark.sql.Window().partitionBy("a").orderBy(["b", "c"])""")
+
     assert (
-        wrapper.visit(PysparkWindowTransformer()).code
-        == 'Window.partitionBy("a", "b").orderBy("c", "d")'
+        module.visit(PysparkWindowTransformer()).code
+        == """Window.partitionBy("a", "b").orderBy("c")
+pyspark.sql.Window().partitionBy("a").orderBy("b", "c")"""
     )
